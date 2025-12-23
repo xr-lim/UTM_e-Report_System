ea223a771c28701fd13d27b295960d08c292e9ae
Huang Bosheng <bosheng@graduate.utm.my>
2025-12-24 00:27:55 +0800

    remove unnecessary comment

--- Laravel/public/chat-proxy.php
+++ Laravel/public/chat-proxy.php
@@ -1,5 +1,4 @@
 <?php
-// DeepSeek API Proxy - Simple PHP file (no Laravel controller)
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, OPTIONS');
